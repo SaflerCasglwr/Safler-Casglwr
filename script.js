a5c4e021f7a98ba3cacc65dee14417749efb4b0d
SaflerCasglwr <emofag69420x@gmail.com>
2025-01-19 20:10:02 +1300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+function start () {
+  // (PART A) GET SPLASH SCREEN 
+  let splash = document.getElementById("splash");
+
+  // (PART B) PLAY BGM & REMOVE SPLASH SCREEN AFTER FADE IN
+  splash.addEventListener("transitionend", () => {
+    document.getElementById("bgm").play();
+    splash.remove();
+  });
+
+  // (PART C) GO!
+  splash.classList.add("hide");
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
